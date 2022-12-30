27fc6b170b74806a824ecc51db8443088e221fa9
Thone <46010284+thonecardoso@users.noreply.github.com>
2022-12-30 19:52:11 -0300

    User entity and resource

---
+++ src/main/java/com/thonecardoso/course/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.thonecardoso.course.resources;
+
+import com.thonecardoso.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        var u = new User(1,"Maria", "Maria@gmail.com", "99999999", "1234");
+        return ResponseEntity.ok().body(u);
+    }
+}
