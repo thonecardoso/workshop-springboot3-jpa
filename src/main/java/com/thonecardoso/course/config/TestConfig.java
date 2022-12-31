a9b571005a306da1dcd5b6f4e1399180828848fb
Thone <46010284+thonecardoso@users.noreply.github.com>
2022-12-31 19:12:13 -0300

    OrderStatus enum

--- src/main/java/com/thonecardoso/course/config/TestConfig.java
+++ src/main/java/com/thonecardoso/course/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.thonecardoso.course.config;
 
 import com.thonecardoso.course.entities.Order;
 import com.thonecardoso.course.entities.User;
+import com.thonecardoso.course.entities.enums.OrderStatus;
 import com.thonecardoso.course.repositories.OrderRepository;
 import com.thonecardoso.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,9 +28,9 @@ public class TestConfig implements CommandLineRunner {
         var u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         var u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-        var o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        var o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        var o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        var o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        var o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        var o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
         userRepository.saveAll(List.of(u1, u2));
         orderRepository.saveAll((List.of(o1, o2, o3)));
