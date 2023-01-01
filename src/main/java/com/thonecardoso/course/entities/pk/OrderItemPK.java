57331c38d0270215486db877f9b5a46d07383002
Thone <46010284+thonecardoso@users.noreply.github.com>
2023-01-01 00:07:54 -0300

    OrderItem, many-to-many association with extra attributes

---
+++ src/main/java/com/thonecardoso/course/entities/pk/OrderItemPK.java
@@ -0,0 +1,52 @@
+package com.thonecardoso.course.entities.pk;
+
+import com.thonecardoso.course.entities.Order;
+import com.thonecardoso.course.entities.Product;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serializable;
+
+@Embeddable
+public class OrderItemPK implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+    @ManyToOne
+    @JoinColumn(name = "product_id")private Product product;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof OrderItemPK that)) return false;
+
+        if (!getOrder().equals(that.getOrder())) return false;
+        return getProduct().equals(that.getProduct());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getOrder().hashCode();
+        result = 31 * result + getProduct().hashCode();
+        return result;
+    }
+}
